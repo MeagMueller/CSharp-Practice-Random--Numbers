a6fe35430da58c44c18a53acb610c30724272c9e
MeagMueller <46437198+MeagMueller@users.noreply.github.com>
2019-05-28 14:00:20 -0500

    Completed exercise

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace random_numbers
 {
@@ -6,7 +7,29 @@ namespace random_numbers
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("C# loves you and thinks you're great.");
+
+            // Use the following code to create a list of random numbers. Each number will be between 0 and 5.
+
+            Random random = new Random();
+            List<int> numbers = new List<int> {
+                random.Next(6),
+                random.Next(6),
+                random.Next(6),
+                random.Next(6),
+                random.Next(6),
+                random.Next(6),
+            };
+
+            for (int i=0; i<numbers.Count; i++) {
+                // Determine if the current loop index is contained inside of the `numbers` list. Print a message to the console indicating whether the index is in the list.
+
+                if (numbers.Contains(i)) {
+                    Console.WriteLine($"numbers list contains {i}");
+                } else {
+                    Console.WriteLine($"numbers list does not contain {i}");
+                }
+            }
         }
     }
 }
